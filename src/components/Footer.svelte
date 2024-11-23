58f9cafc14149aedb11f8ba8e905db464c37207e
Gil Hwang <giyuhwang@gmail.com>
2024-11-23 11:48:09 -0500

    Initial commit for svelte website

---
+++ src/components/Footer.svelte
@@ -0,0 +1,44 @@
+<footer
+  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p>
+      <b class="pr-2">Email</b> il.hwang@mail.utoronto.ca
+    </p>
+    <p>
+      <b class="pr-2">GitHub</b>
+      <a
+        class="text-violet-400"
+        target="_blank"
+        href="https://github.com/gilhwang"
+      >
+        gil_hwang<sup>
+          <span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            ></i>
+          </span></sup
+        >
+      </a>
+    </p>
+    <p>
+      <b class="pr-2">Linkedin</b>
+      <a
+        class="text-violet-400"
+        target="_blank"
+        href="https://www.linkedin.com/in/gil-hwang/"
+      >
+        Gil Hwang<sup>
+          <span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            ></i>
+          </span></sup
+        >
+      </a>
+    </p>
+  </div>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let y;
+  let tabs = [
+    { name: "Projects", link: "#projects" },
+    { name: "About me", link: "#about" },
+    // {name: 'Blog', '#projects'},
+  ];
+  let socialMediaLink = "https://www.linkedin.com/in/gil-hwang/";
+</script>
+
+<header
+  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " +
+    (y > 0
+      ? " py-4 bg-slate-950 border-violet-950"
+      : " py-6 bg-transparent border-transparent")}
+>
+  <h1 class="font-medium">
+    <b font="font-bold poppins">Gil</b> Hwang
+  </h1>
+  <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+    {#each tabs as tab, index}
+      <a href={tab.link} class="duration-200 hover:text-violet-400">
+        <p>{tab.name}</p>
+      </a>
+    {/each}
+    <a
+      href={socialMediaLink}
+      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+      target="_blank"
+    >
+      <div
+        class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
+                    group-hover:translate-x-full z-0 duration-200"
+      />
+      <h4 class="relative z-9">Get in touch</h4>
+    </a>
+  </div>
+</header>
