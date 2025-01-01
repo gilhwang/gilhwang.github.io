6ac74e3335e77e7b4f0add45b838f191ed6e8b3e
Gil Hwang <giyuhwang@gmail.com>
2025-01-01 17:33:50 -0500

    Adjusted font size for mobile

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -25,18 +25,18 @@
     <div
       class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
     >
-      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+      <h2 class="font-semibold text-5xl md:text-6xl">
         Hi! I'm <span class="poppins text-violet-400">Gil</span> Hwang
       </h2>
-      <h3 class="font-semibold text-2xl sm:text-3xl md:text-4xl">I am...</h3>
-      <h3 class="font-base text-lg sm:text-xl md:text-2xl">
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">I am...</h3>
+      <h3 class="font-base text-2xl md:text-3xl">
         a Computer Engineering <span class="poppins text-violet-400"
           >Student</span
         >
         at University of Toronto<br /> & Software
         <span class="poppins text-violet-400">Developer</span> at Ericsson Canada
       </h3>
-      <p class="text-base sm:text-lg md:text-xl">
+      <p class="text-xl sm:text-lg md:text-2xl">
         My <span class="text-violet-400">favorite tech</span> includes C/C++, Python,
         CUDA and Artificial Intelligence
       </p>
@@ -65,15 +65,17 @@
       class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700
       after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-7"
     >
-      <h6 class="text-lg sm:text-xl md:text-2xl">What can I do?</h6>
-      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+      <h6 class="text-xl sm:text-2xl md:text-3xl">What can I do?</h6>
+      <h3 class="font-semibold text-6xl sm:text-5xl">
         My <span class="poppins text-violet-400">Projects</span>
       </h3>
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
       {#each data.posts as post}
         <Step step={post}>
-          <p class="description">{post.description}</p>
+          <p class="description text-lg sm:text-base">
+            {post.description}
+          </p>
         </Step>
       {/each}
     </div>
@@ -86,8 +88,8 @@
       class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700
       after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-7"
     >
-      <h6 class="text-lg sm:text-xl md:text-2xl">What did I do?</h6>
-      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+      <h6 class="text-xl sm:text-2xl md:text-3xl">What did I do?</h6>
+      <h3 class="font-semibold text-6xl sm:text-5xl">
         My <span class="poppins text-violet-400">Experience</span>
       </h3>
     </div>
--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -13,13 +13,17 @@
   >
     <i class={step.icon}></i>
   </div>
-  <h3 class="font-medium text-xl sm:text-2xl md:text-3xl">{step.title}</h3>
+  <h3 class="font-medium text-3xl sm:text-3xl md:text-2xl">
+    {step.title}
+  </h3>
   <slot />
   <div class="flex-1 flex justify-between gap-4 items-center">
     <div
       class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:w-full after:h-full after:bg-white after:duration-200 hover:after:translate-x-full after:z-[-1] overflow-hidden"
     >
-      <p class="relative z-4">Click for more details &rarr;</p>
+      <p class="text-xl sm:text-lg md:text-sm relative z-4">
+        Click for more details &rarr;
+      </p>
     </div>
   </div>
 </a>
